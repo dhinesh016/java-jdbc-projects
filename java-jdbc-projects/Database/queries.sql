e4028fd9e5c7e9e71693176cbf6dc09269c234f2
DHINESH .S <146579799+dhinesh016@users.noreply.github.com>
2026-03-10 23:31:55 +0530

    Create queries.sql

---
+++ java-jdbc-projects/Database/queries.sql
@@ -0,0 +1,14 @@
+/* 
+It is my dbname change the demo want you want
+CREATE DATABASE demo;
+*/
+
+CREATE DATABASE db_name;
+CREATE TABLE restaurant (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(50),
+    location VARCHAR(50),
+    fooditems VARCHAR(100),
+    rating VARCHAR(10),
+    description VARCHAR(200)
+);
